2f0225314ff84cd7374884b1a36e726bd90f5a5e
yuno <hanganda23@naver.com>
2021-10-01 22:20:08 +0900

    style: Search button

--- src/components/Header/SearchForm.tsx
+++ src/components/Header/SearchForm.tsx
@@ -1,20 +1,39 @@
 import styled from "styled-components";
+import searchIcon from '../images/search.png';
 
 function SearchForm() {
   return (
-    <form>
+    <FormContainer>
       <SearchInput type="text" placeholder="무슨 행사를 진행하시나요?" />
-    </form>
+      <SearchButton />
+    </FormContainer>
   );
 }
 
 export default SearchForm;
 
-const SearchInput = styled.input`
-  height: 47px;
+const FormContainer = styled.form`
+  display: flex;
+  align-items: center;
   width: 536px;
   border: 1px solid #0077ed;
   border-radius: 4px;
+  height: 100%;
+`;
+
+const SearchInput = styled.input`
+  flex-grow: 1;
   padding: 9px 20px;
   font-size: 18px;
+  border: 0;
+
+  &::placeholder {
+    color: #b9b9b9;
+  }
 `;
+
+const SearchButton = styled.button`
+  width: 48px;
+  height: 100%;
+  background: url(${searchIcon}) no-repeat center;
+`; 
